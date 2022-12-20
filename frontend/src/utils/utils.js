b16d416674b0fbadfc9503e3cfbdc7a3563424a2
miadahlgren <101955676+miadahlgren@users.noreply.github.com>
2022-12-20 11:14:58 +0100

    removed backslash from url

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://project-auth-4z5bouycqq-lz.a.run.app/";
+const BASE_URL = "https://project-auth-4z5bouycqq-lz.a.run.app";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
